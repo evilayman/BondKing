b0e24ef6c531adfe25c5963d46536fedfa51de82
Ayman M.Ahmed <33355388+evilayman@users.noreply.github.com>
2018-07-21 16:00:26 +0200

    Player, Basic Scene

---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode keyMove, keyJump;
+
+    [SerializeField]
+    private float speed, jumpSpeed;
+
+    [SerializeField]
+    private Transform otherSide;
+
+    [SerializeField]
+    private float angle;
+
+    private Rigidbody rb;
+
+    [SerializeField]
+    private bool isMoving;
+
+    private bool isGrounded;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(keyMove))
+        {
+            isMoving = true;
+        }
+
+        if (Input.GetKeyUp(keyMove))
+        {
+            isMoving = false;
+        }
+
+        if (Input.GetKeyDown(keyJump) && isGrounded)
+        {
+            StartCoroutine(Jump());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            Vector3 dir = (otherSide.position - transform.position).normalized;
+            dir = Quaternion.AngleAxis(angle, Vector3.up) * dir;
+            rb.AddForce(dir * speed);
+        }
+    }
+
+    private IEnumerator Jump()
+    {
+        yield return new WaitForFixedUpdate();
+        rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+        isGrounded = false;
+    }
+
+    public void SwitchState()
+    {
+        isMoving = !isMoving;
+    }
+}
