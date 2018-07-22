e741c5473bea98774267ea283917c557d5001b4c
Nader <nader.ma@live.com>
2018-07-22 23:56:56 +0200

    Added a new scene and a camera script
    Suggestion!
    Why not make it a racing game? or a car game?... hmmm its fun to drive and be fast paced

---
+++ Assets/Scripts/MonoBehaviours/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    [SerializeField]
+    private Transform side1Transform;
+    [SerializeField]
+    private Transform side2Transform;
+
+    private Vector3 lookAtTarget = new Vector3();
+
+    private void Update()
+    {
+        lookAtTarget.x = Mathf.Abs(side1Transform.position.x - side2Transform.position.x) / 2;
+        lookAtTarget.y = Mathf.Abs(side1Transform.position.y - side2Transform.position.y) / 2;
+        lookAtTarget.z = Mathf.Abs(side1Transform.position.z - side2Transform.position.z) / 2;
+
+        transform.LookAt(lookAtTarget);
+
+    }
+
+}
